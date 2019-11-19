dc7777c535cd8af2bfaeef9e13bd4b013396bc66
Jules Rasetaharison <Jules.Rasetaharison@auticon.de>
2019-11-19 21:46:46 +0100

    includes and associated warnings fixed, all tests passed

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,11 @@
 using Documenter, AgnosticBayesEnsemble
 
 makedocs( modules=[AgnosticBayesEnsemble],
-         doctest=true )
+          doctest=true, 
+          sitename="AgnosticBayesEnsemble", 
+          format = :html )
 
-         
+deploydocs( deps = Deps.pip("mkdocs", "python-markdown-math"),
+            repo = "github.com/hondoRandale/AgnosticBayesEnsemble.jl",
+            julia  = "1.2.0",
+            osname = "linux" )        
