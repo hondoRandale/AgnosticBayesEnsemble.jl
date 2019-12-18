78a1eb8c24eb6326d2090565dacd71dbb4d9a25b
Jules Rasetaharison <Jules.Rasetaharison@auticon.de>
2019-12-18 15:05:10 +0100

    argminProb( r::Matrix{Float64} ) fixed.

--- test/unitTestargminProb.jl
+++ test/unitTestargminProb.jl
@@ -16,4 +16,12 @@ using Test
 
   @test maxVal / minVal > 0.95
   @test maxVal / minVal < 1.05 
+
+  len        = 20;
+  lmatrix    = repeat( [2.0], outer=[len 3] );
+  rmatrix    = rand( 4.0:20.0, len, 3 );
+  vmatrix    = hcat( lmatrix, rmatrix );
+  minIndices = argminProb( vmatrix );
   
+  @test all( minIndices .>= 1 )
+  @test all( minIndices .<= 3 )
