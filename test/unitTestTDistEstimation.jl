7a4e920fdeec6ac91f54f9d50308624b0252bd2d
Jules Rasetaharison <Jules.Rasetaharison@auticon.de>
2020-01-13 14:16:28 +0100

    changed t-dist unit test to avoid timeouts on CI server, version changed to 0.16

--- test/unitTestTDistEstimation.jl
+++ test/unitTestTDistEstimation.jl
@@ -16,7 +16,7 @@ posterior = TDistPosteriorEstimationReference( errMatTraining, 10000 );
 @test all( posterior .<= 1.0 );
 @test all( sum( posterior ) ≈ 1.0 );
 
-posterior = TDistPosteriorEstimation( errMatTraining, 10000000, κ_0 = 2000000000.0, v_0 = 0.05, α = 0.000000000001 );
+posterior = TDistPosteriorEstimation( errMatTraining, 1000000, κ_0 = 200000000.0, v_0 = 0.05, α = 0.00000000001 );
 
 @test all( posterior .>= 0.0 );
 @test all( posterior .<= 1.0 );
