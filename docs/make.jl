123df829c6e2b9b94951b3f52491a8c11e9d1134
Jules Rasetaharison <Jules.Rasetaharison@auticon.de>
2020-01-21 19:38:27 +0100

    doc build set up.

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,7 @@ using Documenter, AgnosticBayesEnsemble
 makedocs( sitename = "AgnosticBayesEnsemble",
           doctest  = true );
 
+deploydocs(
+  repo = "github.com/hondoRandale/AgnosticBayesEnsemble.jl.git",
+);
+
